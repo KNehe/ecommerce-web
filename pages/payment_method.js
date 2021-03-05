95af4f1cacf74e99f03d5f55948a5b0f5996372e
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-03-05 21:54:40 +0300

    Payment method screen design and refactor

--- pages/payment_method.js
+++ pages/payment_method.js
@@ -7,6 +7,11 @@ import { getShippingCost, getTax, sendPayPalOrder, sendStripeOrder } from "../ex
 import { PAY_PAL, STRIPE_PAYMENT, USER_TYPE_GUEST, USER_TYPE_RESGISTERED } from '../consts/index'
 import { useRouter } from 'next/router'
 import { CardElement, useStripe, useElements} from '@stripe/react-stripe-js'
+import styles from '../styles/PaymentMethod.module.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCreditCard } from "@fortawesome/free-solid-svg-icons";
+import { faPaypal } from "@fortawesome/free-brands-svg-icons";
+import ProgressIndicator from './../components/progress_indicator/progress_indicator'
 
 const PaymentMethod = () =>{
     
@@ -21,20 +26,28 @@ const PaymentMethod = () =>{
     const stripe = useStripe()
 
     const elements = useElements()
+
+    const { cart, ShippingDetails, userId } = state
     
-    useEffect(()=>{
+    useEffect( ()=>{
+        if(cart.length === 0){
+            return router.push('/')
+        }
         dispatch({type: SET_NAVBAR_TITLE, payload: 'Choose payment method'})
+        setCostsAttached()
     },[])
     
-    const { cart, ShippingDetails, userId } = state
+    const [ costs, setCosts ] = useState({ tax:'', shippingCost:'', total:'', totalItemPrice:'' })
 
-    const getCostsAttached = async () =>{
+    const [ isSendingOrder, setIsSendingOrder ] = useState(false)
+
+    const setCostsAttached = async () =>{
       const tax = await getTax();
       const shippingCost = await getShippingCost();
-      const total = cart.reduce((prev,item) => prev + item.price,0)
-      const totalItemPrice = tax + shippingCost + total
-
-      return { tax, shippingCost, total, totalItemPrice }
+      const totalItemPrice = cart.reduce((prev,item) => prev + item.price,0)
+      const total = tax + shippingCost + totalItemPrice
+    
+      return setCosts( { tax, shippingCost, total, totalItemPrice })
     }
 
     const createOrderPayload = (tax, shippingCost, total, totalItemPrice,paymentMethod) =>{
@@ -80,21 +93,22 @@ const PaymentMethod = () =>{
                 client: clientInstance
             })
             
-            const { tax, shippingCost, total, totalItemPrice} = await getCostsAttached()
-
             const payload = await paypalInstance.tokenize({
                 flow:'checkout',
-                amount: totalItemPrice,
+                amount: costs.totalItemPrice,
                 currency:'USD',
                 displayName: 'NTrade'
             })
             
-            const order = createOrderPayload(tax, shippingCost, total, totalItemPrice, PAY_PAL)
+            const order = createOrderPayload(costs.tax, costs.shippingCost, costs.total, costs.totalItemPrice, PAY_PAL)
+            
+            setIsSendingOrder(true)
 
             const { result, errorMsg} = await sendPayPalOrder( order, payload.nonce)
 
             if(errorMsg){
                 setError(errorMsg)
+                setIsSendingOrder(false)
                 return setButtonDisableProp(false)
             }
 
@@ -106,6 +120,7 @@ const PaymentMethod = () =>{
 
         }catch(error){
             console.error("Error", error)
+            setIsSendingOrder(true)
             if(error.code === 'PAYPAL_POPUP_CLOSED'){
                 setError("Process cancelled")
                 return setButtonDisableProp(false)
@@ -119,8 +134,7 @@ const PaymentMethod = () =>{
 
     const onStripeFormSubmittedHandler = async event =>{
         event.preventDefault()
-        
-        setButtonDisableProp(true)
+        setIsSendingOrder(true)
 
         setError('')
 
@@ -133,15 +147,16 @@ const PaymentMethod = () =>{
 
        if(!error){
         const { id } = paymentMethod;
-
-        const { tax, shippingCost, total, totalItemPrice} = await getCostsAttached()
         
-        const order = createOrderPayload(tax, shippingCost, total, totalItemPrice, STRIPE_PAYMENT)
+        const order = createOrderPayload(costs.tax, costs.shippingCost, costs.total, costs.totalItemPrice, STRIPE_PAYMENT)
+        
+        setIsSendingOrder(true)
 
         const { errorMsg, result} = await sendStripeOrder(id,order)
 
         if(errorMsg){
             setButtonDisableProp(false)
+            setIsSendingOrder(false)
             return setError(errorMsg)
         }else{
             dispatch({type: SET_SINGLE_ORDER, payload: result})
@@ -153,27 +168,69 @@ const PaymentMethod = () =>{
         
        }else{
         setButtonDisableProp(false)
+        setIsSendingOrder(false)
         return setError(error.message)
        }
     }catch(err){
         setButtonDisableProp(false)
+        setIsSendingOrder(false)
         return setError(error.message)
     }
     }
 
     return <Layout title='Payment method'>
+       <section className={styles.main}>
         <h1>Order summary</h1>
-        {error}
-        <button 
-            onClick={onPayPalButtonClickedHandler}
-            disabled={ btnDisabled}>Payment method
-        </button>
-        <form onSubmit={onStripeFormSubmittedHandler}>
-            <CardElement/>
-            <button type='submit' disabled={btnDisabled}>
-                Pay
-            </button>
-        </form>
+        <div className={styles.group}>
+            <div className={styles.row}>
+                <p>Items</p>
+                <p>${costs.totalItemPrice}</p>
+            </div>
+            <div className={styles.row}>
+                <p>Shipping</p>
+                <p>${costs.shippingCost}</p>
+            </div>
+            <div className={styles.row}>
+                <p>Tax</p>
+                <p>${costs.tax}</p>
+            </div>
+            <div className={styles.row}>
+                <p>Total</p>
+                <p>${costs.total}</p>
+            </div>
+        </div>
+        {
+            isSendingOrder?           
+            <div className={styles.progress_indicator_wrapper}>
+                <ProgressIndicator/>
+            </div>:
+            <>
+                <h2>Choose payment method</h2>
+
+            <div className={styles.form_section}>
+                <p className={styles.error_message}>
+                    {error}
+                </p>
+                <button 
+                    onClick={onPayPalButtonClickedHandler}
+                    disabled={ btnDisabled}
+                    className={styles.paypal_btn}>
+                        <FontAwesomeIcon icon={faPaypal}/>
+                        <span className={styles.txt_1}>Pay</span>Pal
+                </button>
+                <p className={styles.or}>Or</p>
+                <form onSubmit={onStripeFormSubmittedHandler}>
+                    <CardElement/>
+                    <button type='submit' disabled={btnDisabled}  className={styles.stripe_btn}>
+                    <FontAwesomeIcon icon={faCreditCard}/>  Credit or Debit card
+                    </button>
+                </form>
+        </div>
+            
+            </>
+        }
+       
+       </section>
     </Layout>
 
 }
