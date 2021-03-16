505af81025a92b51b70f5881aebbd023e9e40b6c
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-03-16 14:35:37 +0300

    Hide cart on some pages

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -5,6 +5,7 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import Link  from 'next/link'
 import { SET_NAVBAR_TITLE } from '../../state/actions'
 import { useDispatch, useSelector } from 'react-redux'
+import { PRODUCT_DETAILS, SHOPPING_CART, STORE } from '../../consts/navbar_titles'
 
 
 const NavBar = ()=>{
@@ -22,12 +23,17 @@ const NavBar = ()=>{
             >
                 <p>{navBarTitle}</p>
                 <Link href='/shopping_cart'>
-                    <div 
+                    {
+                        navBarTitle === STORE ||
+                        navBarTitle ===  SHOPPING_CART ||
+                        navBarTitle === PRODUCT_DETAILS ?
+                        <div 
                         className={styles.cart_icon_group} 
                         onClick={onCartIconClickedHandler} >
                         <FontAwesomeIcon icon={faShoppingCart}/>
                         <p className={styles.cart_count}>{cart.length}</p>
-                    </div>
+                       </div>: ''
+                    }
                 </Link>
             </nav>
 }
