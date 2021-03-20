21db06e79fbdb67f148619301c79964883da7a64
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-03-20 12:56:27 +0300

    Set current activity on view single order initiation on thank_you screen

--- pages/thank_you.js
+++ pages/thank_you.js
@@ -5,25 +5,38 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import Layout from "../components/layout/layout";
 import { SUCCESS_ORDER } from "../consts/navbar_titles";
-import { SET_NAVBAR_TITLE } from "../state/actions";
+import { SET_CURRENT_ACTIVITY, SET_NAVBAR_TITLE } from "../state/actions";
 import styles from '../styles/ThankYou.module.scss'
+import { useRouter  } from "next/router";
+import { VIEWING_SINGLE_ORDER } from "../consts/activities";
 
 const ThankYou = () =>{
 
     const dispatch = useDispatch()
 
+    const router = useRouter()
+
     useEffect(()=>{
         dispatch({type: SET_NAVBAR_TITLE, payload: SUCCESS_ORDER})
+
+        router.prefetch('/single_order')
     },[])
+
+    const onBtnClickedHandler = async event =>{
+
+        event.preventDefault()
+
+        dispatch({type: SET_CURRENT_ACTIVITY, payload: VIEWING_SINGLE_ORDER})
+
+        await router.push('/single_order')
+    }
 
     return <Layout title='Thank you'>
 
         <section className={styles.thank_you}>
             <FontAwesomeIcon icon={faStar}/>
             <p>Payment made successfully</p>
-            <Link href='/single_order'>
-                <button>View Order</button>
-            </Link>
+                <button onClick={onBtnClickedHandler}>View Order</button>
         </section>
 
     </Layout>
